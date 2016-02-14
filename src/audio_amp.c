683746e10ab65cb758a68cd0863c8e5b36a58dbb
mplanton <manuel.planton@gmail.com>
2016-02-14 15:55:38 +0100

    added structure for main program

---
+++ src/audio_amp.c
@@ -0,0 +1,23 @@
+// audio_amp.c
+// the main program 'audio_amp'
+
+#include "amp_functions.h"
+
+// USAGE: audio_amp <input soundfile> <amp-factor>
+
+int main(int argc, char **argv)
+{
+  testPrint();
+  
+  // check arguments
+  
+  // initialize stuff
+  
+  // open files
+  
+  // main loop
+  
+  // clean up
+  
+  return 0;
+}
